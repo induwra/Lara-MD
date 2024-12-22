4a3579c53fdf932c7abe1cc3ca670e178742bc32
induwra <induwaraisira793@gmail.com>
2024-12-22 10:36:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=qA1m2bQD#1a_4mEJ29uFFK-5TObKkcj0dHglIg9yRBPhHQPujFhc", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
